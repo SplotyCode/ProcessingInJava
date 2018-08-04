bde36419b88bc1bea3984a803d109e56a4b7bdd7
david <davidscandurra@gmail.com>
2018-08-04 17:46:59 +0200

    Events Clean Up

--- src/main/java/me/david/processinginjava/application/Events.java
+++ src/main/java/me/david/processinginjava/application/Events.java
@@ -3,18 +3,23 @@ package me.david.processinginjava.application;
 import me.david.processinginjava.Application;
 import me.david.processinginjava.input.*;
 import org.lwjgl.glfw.GLFW;
-import org.lwjgl.glfw.GLFWCursorPosCallback;
 
 public class Events {
 
     public Events(long window, Application application) {
+        /* Window Events/Callbacks */
         GLFW.glfwSetWindowFocusCallback(window, new WindowFocusHandler(application));
         GLFW.glfwSetWindowSizeCallback(window, new WindowResizeHandler(application));
-        GLFW.glfwSetKeyCallback(window, (windowID, key, scancode, action, mods) -> {
 
-        });
+        /* Mouse Related Events/Callbacks */
         GLFW.glfwSetMouseButtonCallback(window, new MouseButtonHandler(application));
         GLFW.glfwSetScrollCallback(window, new MouseScrollHandler(application));
         GLFW.glfwSetCursorPosCallback(window, new MousePositionHandler(application));
+
+        /* Keyboard Events/Callbacks */
+        GLFW.glfwSetKeyCallback(window, (windowID, key, scancode, action, mods) -> {
+
+        });
+
     }
 }
